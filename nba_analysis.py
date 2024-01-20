c768c1f8c3e0f503a86f65b1102b81f744851c4d
SadafAsad <sadaf98x@gmail.com>
2024-01-20 13:20:37 -0500

    dataframe test

--- nba_analysis.py
+++ nba_analysis.py
@@ -2,13 +2,12 @@
 # How to create a dataframe from a dictionary
 # ---------------------------------------------
 import pandas as pd
-import matplotlib.pyplot as plt
 # Notice key and value pairs
 # Keys are in simple string format and values are in array format
 dict_={'a':[11,21,31],'b':[12,22,32]}
 df=pd.DataFrame(dict_)
-type(df)
-df.head()
-df.mean()
+print(f"type(df): \n{type(df)}\n")
+print(f"df.head(): \n{df.head()}\n")
+print(f"df.mean(): \n{df.mean()}\n")
 
 
