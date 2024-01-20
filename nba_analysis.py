4616b23f684e1a3fabbe8a79f7751010873f791d
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-20 13:52:39 -0500

    testing nba api

--- nba_analysis.py
+++ nba_analysis.py
@@ -1,14 +1,17 @@
-# ---------------------------------------------
-# How to create a dataframe from a dictionary
-# ---------------------------------------------
+# import section
 import pandas as pd
-# Notice key and value pairs
-# Keys are in simple string format and values are in array format
-dict_={'a':[11,21,31],'b':[12,22,32]}
-df=pd.DataFrame(dict_)
-print(f"type(df): \n{type(df)}\n")
-print(f"df.head(): \n{df.head()}\n")
-print(f"df.mean(): \n{df.mean()}\n")
+from nba_api.stats.static import teams
+
+# # ---------------------------------------------
+# # How to create a dataframe from a dictionary
+# # ---------------------------------------------
+# # Notice key and value pairs
+# # Keys are in simple string format and values are in array format
+# dict_={'a':[11,21,31],'b':[12,22,32]}
+# df=pd.DataFrame(dict_)
+# print(f"type(df): \n{type(df)}\n")
+# print(f"df.head(): \n{df.head()}\n")
+# print(f"df.mean(): \n{df.mean()}\n")
 
 # ----------------------------------------------------------------------------------
 # Constructing a dictionary from an array of dictionaries
@@ -22,4 +25,8 @@ def to_dict(list_dict):
         for key, value in dict_.items():
             out_dict[key].append(value)
     return out_dict
+
+# check what the returned data look like
+nba_teams = teams.get_teams()
+print(nba_teams[0:3])
 
